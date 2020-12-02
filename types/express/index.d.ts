8f24c940fe73d32f207e2d18a2ba8dd34a1615e7
Famil Restu Pambudi <famil.pambudi@gmail.com>
2020-12-02 21:46:50 +0700

    no more global variables, migrating to module aliases

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,14 +1,6 @@
-// import { Express as ExpressTypes } from 'express';
-
 declare namespace Express {
     export interface Request {
         isAppRoutes?: boolean;
         currentApp?: string;
-    }
-}
-
-declare namespace NodeJS {
-    export interface Global {
-        Core: any;
     }
 }
