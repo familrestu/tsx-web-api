d88bf1448ed1a7aeb23f28d5f76b4e8b97017f54
Famil Restu Pambudi <famil.pambudi@gmail.com>
2020-12-02 21:47:21 +0700

    testing

--- src/api/components/ExampleTable.ts
+++ src/api/components/ExampleTable.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-import Core, { ListingReturnType } from '@system/core';
+import Base, { TableDataReturnType } from '@system/base';
 
-class ExampleTable extends Core {
-    Listing(req: express.Request): ListingReturnType {
-        const arrHeader = [];
-        const arrBody = [];
+class ExampleTable extends Base {
+    TableData(req: express.Request): TableDataReturnType {
+        const arrHeader: string[] = [];
+        const arrBody: string[][] = [];
 
         for (let i = 1; i <= 20; i++) {
             arrHeader.push(`column_${i}`);
 
             const tempArrBody = [];
-            for (let x = 0; x < 500; x++) {
+            for (let x = 0; x < 50; x++) {
                 tempArrBody.push('Lorem Ipsum');
             }
 
