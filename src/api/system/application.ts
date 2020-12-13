9f84f1d61847ee7c03c31f91512313c07b4967f3
Famil Restu Pambudi <famil.pambudi@gmail.com>
2020-12-13 12:11:50 +0700

    update jwt payload type

--- src/api/system/application.ts
+++ src/api/system/application.ts
@@ -6,6 +6,18 @@ import moment from 'moment';
 
 import { query } from '@database';
 
+type JWTPayloadType = {
+    // user_id: number;
+    // app_id: number;
+    // activated_app: string[];
+    // default_app: string;
+    // current_app: string;
+    // username: string;
+    // email: string;
+    // full_name: string;
+    [key: string]: string | string[] | number | number[];
+};
+
 type MenuAuthReturnType = {
     menuData: {
         group: string | null;
@@ -23,9 +35,35 @@ type MenuAuthReturnType = {
     }[];
 };
 
+type AuthReturnType = {
+    loginStatus: boolean;
+    refreshToken?: boolean;
+    error?: TypeError;
+    message?: string;
+};
+
 class Global {
+    SetJWT(req: express.Request, res: express.Response, data: JWTPayloadType) {
+        const expiredNumber = 15 * 60;
+        const iat = Math.floor(Date.now() / 1000);
+        const exp = Date.now() / 1000 + expiredNumber;
+        const uuid = uuidv5(iat.toString(), `${process.env.UUID_NAMESPACE as string}`);
+        const jwtSignature = hash.sha256().update(`${uuid}${process.env.JWT_KEY}`).digest('hex');
+        const jwtPayload = {
+            // user_id: data.user_id,
+            iss: req.headers.host,
+            sub: data.username || data.email,
+            data,
+            iat,
+            exp: Math.floor(exp),
+        };
+
+        res.cookie('jwt', jwt.sign(jwtPayload, jwtSignature), { httpOnly: true, signed: true, sameSite: 'lax', expires: new Date(Date.now() + expiredNumber * 1000) });
+        res.cookie('uuid', uuid, { httpOnly: true, signed: true, sameSite: 'lax', expires: new Date(Date.now() + expiredNumber * 1000) });
+    }
+
     Login(req: express.Request, res: express.Response) {
-        let result: any = {
+        const result: any = {
             status: false,
         };
 
@@ -33,7 +71,7 @@ class Global {
             (req.body.email === 'famil.restu@ersys.com' && req.body.password === 'password') ||
             (req.body.is_accountcode !== undefined && req.body.username === 'famil.restu' && req.body.password === 'password')
         ) {
-            const data = {
+            const data: JWTPayloadType = {
                 user_id: 1,
                 app_id: 1,
                 activated_app: ['hris'],
@@ -42,33 +80,29 @@ class Global {
                 username: 'famil.restu',
                 email: 'famil.restu@ersys.com',
                 full_name: 'Famil Restu Pambudi',
-                profile_picture: 'https://lh3.googleusercontent.com/ogw/ADGmqu8Xth9CuZj0MrKx-cdFhmJXKFCCr9eEwgIy4qci1A=s83-c-mo',
+                // profile_picture: 'https://lh3.googleusercontent.com/ogw/ADGmqu8Xth9CuZj0MrKx-cdFhmJXKFCCr9eEwgIy4qci1A=s83-c-mo',
             };
 
-            const uuid = uuidv5(data.email, `${process.env.UUID_NAMESPACE as string}`);
+            // const expiredNumber = 60 * 60;
+            // const iat = Math.floor(Date.now() / 1000);
+            // const exp = Date.now() / 1000 + expiredNumber;
+            // const uuid = uuidv5(iat.toString(), `${process.env.UUID_NAMESPACE as string}`);
+            // const jwtSignature = hash.sha256().update(`${uuid}${process.env.JWT_KEY}`).digest('hex');
+            // const jwtPayload = {
+            //     // user_id: data.user_id,
+            //     iss: req.headers.host,
+            //     sub: data.username || data.email,
+            //     data,
+            //     iat,
+            //     exp: Math.floor(exp),
+            // };
+            // result.jwt = jwt.sign(jwtPayload, jwtSignature);
+            // res.cookie('jwt', jwtString, { httpOnly: true, signed: true, sameSite: 'lax', expires: new Date(Date.now() + expiredNumber * 1000) });
+            // res.cookie('uuid', uuid, { httpOnly: true, signed: true, sameSite: 'lax', expires: new Date(Date.now() + expiredNumber * 1000) });
+            // result = { ...result, ...data, uuid };
 
-            const iat = Math.floor(Date.now() / 1000);
-            const exp = Date.now() / 1000 + 60 * 60;
-
-            const jwtSignature = hash.sha256().update(`${uuid}${process.env.JWT_KEY}`).digest('hex');
-
-            const jwtPayload = {
-                // user_id: data.user_id,
-                iss: req.headers.host,
-                sub: data.username || data.email,
-                data,
-                iat,
-                exp: Math.floor(exp),
-            };
-
-            result.jwt = jwt.sign(jwtPayload, jwtSignature);
-
-            res.cookie('jwt', result.jwt, { httpOnly: true, signed: true, sameSite: 'lax' });
-            res.cookie('uuid', uuid, { httpOnly: true, signed: true, sameSite: 'lax' });
-
+            this.SetJWT(req, res, data);
             result.loginStatus = true;
-
-            result = { ...result, ...data, uuid };
         }
 
         return { ...result };
@@ -238,19 +272,23 @@ class Global {
         };
     }
 
-    LoginStatus(req: express.Request, res: express.Response) {
-        let result: {
-            loginStatus: boolean;
-            error?: TypeError;
-            message?: string;
-        } = {
+    VerifyJWT(uuid: string, jwtString: string) {
+        const jwtSignature = hash.sha256().update(`${uuid}${process.env.JWT_KEY}`).digest('hex');
+        const decoded: any = jwt.verify(jwtString, jwtSignature);
+
+        return decoded;
+    }
+
+    LoginStatus(req: express.Request) {
+        let result: AuthReturnType = {
             loginStatus: false,
         };
 
         if (req.signedCookies.jwt && req.signedCookies.uuid) {
             try {
-                const jwtSignature = hash.sha256().update(`${req.signedCookies.uuid}${process.env.JWT_KEY}`).digest('hex');
-                const decoded: any = jwt.verify(req.signedCookies.jwt, jwtSignature);
+                // const jwtSignature = hash.sha256().update(`${req.signedCookies.uuid}${process.env.JWT_KEY}`).digest('hex');
+                // const decoded: any = jwt.verify(req.signedCookies.jwt, jwtSignature);
+                const decoded = this.VerifyJWT(req.signedCookies.uuid, req.signedCookies.jwt);
 
                 result = { ...result, ...decoded.data, loginStatus: true };
             } catch (error) {
@@ -266,8 +304,33 @@ class Global {
         return { ...result };
     }
 
-    GetToken(req: express.Request) {
-        const result = {};
+    GetToken(req: express.Request, res: express.Response) {
+        const result: AuthReturnType = {
+            loginStatus: false,
+        };
+
+        try {
+            if (req.signedCookies.jwt && req.signedCookies.uuid) {
+                const decoded = this.VerifyJWT(req.signedCookies.uuid, req.signedCookies.jwt);
+                const currentTime = moment();
+                const expiredAt = moment(decoded.exp * 1000);
+                const tokenTimeLeft = Math.round(expiredAt.diff(currentTime) / 1000);
+
+                console.log(`user_id: ${decoded.data.user_id}, app_id: ${decoded.data.app_id}, available: ${(tokenTimeLeft / 60).toFixed(2)} minutes left`);
+
+                /* 10 sec left, then renew token */
+                if (tokenTimeLeft < 10) {
+                    this.SetJWT(req, res, decoded.data);
+                    result.refreshToken = true;
+                }
+
+                result.loginStatus = true;
+            }
+        } catch (error) {
+            result.loginStatus = false;
+            result.error = error;
+            result.message = error.message;
+        }
 
         return result;
     }
