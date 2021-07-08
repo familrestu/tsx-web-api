2f45a5eb2ccc093ab2ad3d3958dc6a8aa67786b0
Famil Restu Pambudi <famil.pambudi@gmail.com>
2021-07-08 20:21:56 +0700

    update EmpInfo

--- src/api/hris/employee/employeeInformation.ts
+++ src/api/hris/employee/employeeInformation.ts
@@ -1,5 +1,33 @@
-class EmployeeInformation {
-    /*  */
+import express from 'express';
+import Base from '@system/base';
+import { query } from '@database';
+
+class EmployeeInformation extends Base {
+    async TableData(req: express.Request) {
+        return await this.Listing(req);
+    }
+
+    async Listing(req: express.Request) {
+        let result: any = {};
+        try {
+            const qMenu = await query(
+                `
+                select      emp_no as employee_no, full_name, full_name, position, department, division, join_date, grade
+                from        view_employee
+                order by    emp_no
+                `,
+                [],
+                req.datasource.admin,
+            );
+
+            result = await super.SetListing(req, qMenu, req.body);
+        } catch (error) {
+            result.status = false;
+            result.stack = error;
+        }
+
+        return result;
+    }
 }
 
 export default new EmployeeInformation();
